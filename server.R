c4085a81bdb63bbfc69256217b71c0071c13cd7d
gkonechne <113539763+gkonechne@users.noreply.github.com>
2022-11-29 15:26:41 -0600

    Grace's tab
    ui.R - updated tabPanel "Summaries"
    server.R - added output$SelectYear and output$Grace_table for my "Summaries" tab
    global.R - added in Caleb's file

--- server.R
+++ server.R
@@ -7,14 +7,11 @@ library(tigris) ## For downloading the zipcode map
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-
-
-
+  
+  
+  
   ## Text Outputs
-  output$txtOutput <- renderText({
-    paste0("Open Payments: Payments that drug & medical device companies 
-           make to covered recipients (physicians, nurses, etc). ")
-  })
+  
   output$txtOutput2 <- renderText({
     paste0("Nature of Payments: Categories describing what form or type 
            of payment was made.")
@@ -23,27 +20,36 @@ server <- function(input, output, session) {
     paste0("Amount: For each zipcode, a cumulative total of the dollar amount
            from every payment over the years 2013-18.")
   })
-
+  output$txtOutput4 <- renderText({
+    paste0("List of countries, except the US, who made payments.")
+  })
+  
+  
   output$Gracetxt <- renderText({
     paste0("Summary Payments: Summary statistics for payments in each category 
           of doctor for each year from 2013 to 2018.")
-
+  })
   output$Emmatxt <- renderText({
     paste0("Total Payment Amount by Payment Type and Profession")
-
   })
-
-
-
-
+  
+  output$Abouttxt <- renderText({
+    paste0("Open Payments: Payments that drug & medical device companies 
+           make to covered recipients (physicians, nurses, etc). 
+           Learn more at https://www.cms.gov/openpayments")
+  })
+  
+  
+  
+  
   ## 'Select City' Output
   output$city <- renderUI({
     selectInput("city", "Select City", choices = cities)
   })
-
-
-
-
+  
+  
+  
+  
   ## Plot Outputs
   output$donut_plot <- renderPlotly({
     
@@ -53,7 +59,7 @@ server <- function(input, output, session) {
     
     ## using input for city
     donutdata <- filter(payment, recipient_city == input$city)
-
+    
     ## create dataframe
     donutdata2 <- data.frame(
       cat = levels(donutdata$nature.of.payment),
@@ -63,15 +69,15 @@ server <- function(input, output, session) {
       )
     )
     donutdata3 <- donutdata2 %>% drop_na(count)
-
+    
     ## compute ymax and ymin
     donutdata3$fraction <- donutdata3$count / sum(donutdata3$count)
     donutdata3$ymax <- cumsum(donutdata3$fraction)
     donutdata3$ymin <- c(0, head(donutdata3$ymax, n = -1))
-
+    
     ## change font
     t <- list(family = "Arial", size = 16, color = "black")
-
+    
     ## plotting
     donutdata3 <- donutdata3 %>% group_by(cat)
     fig <- donutdata3 %>%
@@ -85,38 +91,38 @@ server <- function(input, output, session) {
         legend = list(title = list(text = "Type:", font = t))
       )
   })
-
-
-
-
+  
+  
+  
+  
   output$sd_map <- renderPlot({
-
+    
     ## create dataframe
     yourdata <- data.frame(
       ZCTA5CE10 = mapdata$zippy,
       Amount = mapdata$total_amount_of_payment_usdollars
     )
-
+    
     ## download a shapefile (shp,gpkg,geojson...)
     options(tigris_use_cache = TRUE)
     geo <- st_as_sf(zctas(cb = FALSE, state = "South Dakota", year = 2010))
-
+    
     ## overall shape of the state
     state <- st_as_sf(zctas(cb = FALSE, state = "South Dakota", year = 2010))
     state <- st_transform(state, st_crs(geo))
-
+    
     ## merge the data
     yourdata.sf <- merge(geo, yourdata)
-
+    
     ## plotting
     par(mar = c(1, 1, 1, 1))
     ghostLayer(yourdata.sf)
     plot(st_geometry(state), add = TRUE, border = "gray")
     choroLayer(yourdata.sf,
-      var = "Amount",
-      add = TRUE,
-      border = NA,
-      legend.pos = "n"
+               var = "Amount",
+               add = TRUE,
+               border = NA,
+               legend.pos = "n"
     )
     legendChoro(
       pos = "bottomleft", title.txt = "Amount",
@@ -129,5 +135,83 @@ server <- function(input, output, session) {
     )
   })
   
+  
+  
+  output$country <- renderPlot({
+    ggplot(df2, aes_string(input$predictors)) +
+      geom_bar(aes(fill = df2$'Applicable_Manufacturer_or_GOP_Making_Payment_Country')) +
+      theme(axis.title.y = element_blank(),
+            axis.text.y = element_blank(),
+            axis.ticks.y = element_blank(),
+            axis.title.x = element_blank(),
+            axis.text.x = element_text(size = 12),
+            legend.text = element_text(size = 19),
+            legend.title = element_text(size = 20)) +
+      guides(fill = guide_legend(title = "Country"))
+    
+  })
+  
+  
+  ## Select Year Output
+  output$SelectYear <- renderUI({
+    selectInput("SelectYear", "Select Year", choices = 2013:2018)
+  })
+  
+  output$Grace_table <- renderDataTable({
+    
+    req(input$SelectYear) 
+    
+    table_data <- filter(total_pay_data, year == input$SelectYear)
+    
+    #find sum of all payments for each doctor type
+    totalPayPerType <- aggregate(
+      total_amount_of_payment_usdollars ~ physician_primary_type,
+      data = table_data,
+      FUN = sum
+    )
+    
+    medPayPerType <- aggregate(
+      total_amount_of_payment_usdollars ~ physician_primary_type,
+      data = table_data,
+      FUN = median
+    )
+    
+    
+    maxPayPerType <- aggregate(
+      total_amount_of_payment_usdollars ~ physician_primary_type,
+      data = table_data,
+      FUN = max
+    )
+    
+    
+    minPayPerType <- aggregate(
+      total_amount_of_payment_usdollars ~ physician_primary_type,
+      data = table_data,
+      FUN = min
+    )
+    
+    #Combine all columns into one
+    fulltable <- merge(totalPayPerType, 
+                       medPayPerType, 
+                       by = "physician_primary_type") %>%
+      merge(maxPayPerType, by = "physician_primary_type" ) %>%
+      merge(minPayPerType, by = "physician_primary_type" )
+    
+    
+    #Change column names
+    colnames(fulltable) <- c("Physician Type",
+                             "Total Payment Amount",
+                             "Most Common Payment",
+                             "Max Payment", 
+                             "Min Payment")
+    
+    #Print as table
+    datatable(fulltable,
+              options = list(orderClasses = TRUE),
+              rownames = FALSE) %>%
+      formatCurrency(2:5) %>% 
+      formatRound(2:5, 2)
+    
+  })
   
 }
--- ui.R
+++ ui.R
@@ -8,73 +8,90 @@ library(DT)
 
 # Define UI for application that draws a histogram
 fluidPage(
-
-
+  
+  
   # Application title
-
-  titlePanel("Open Payments In South Dakota (2013-2018)"
+  
+  titlePanel("Doctor's Payments In South Dakota (2013-2018)"
   ),
   
-  # Sidebar with a select input for City
-  sidebarLayout(
-
-
-    sidebarPanel(verbatimTextOutput("txtOutput"), width = 0),
-    mainPanel(
-
-
-      ## Text Output and Styles
-      tags$head(
-        tags$style("#txtOutput{color: steelblue;
-                                 font-size: 18px;
-                                 font-style: bold;
-                                 font-family: Arial;
-                                 }"),
-        tags$style("#txtOutput2{color: steelblue;
+  # Main Panel
+  
+  mainPanel(
+    
+    
+    ## Text Output and Styles
+    tags$head(
+      tags$style("#txtOutput2{color: steelblue;
                                  font-size: 17px;
                                  font-style: bold;
                                  font-family: Arial;
                                  }"),
-        tags$style("#txtOutput3{color: steelblue;
+      tags$style("#txtOutput3{color: steelblue;
                                  font-size: 17px;
                                  font-style: bold;
                                  font-family: Arial;
                                  }"),
-        tags$style("#city{font-size: 17px;}")
+      tags$style("#txtOutput4{color: steelblue;
+                                 font-size: 17px;
+                                 font-style: bold;
+                                 font-family: Arial;
+                                 }"),
+      tags$style("#city{font-size: 17px;}")
+    ),
+    
+    
+    ## Tabs Panel
+    tags$head(
+      tags$style(
+        type = "text/css",
+        ".nav-tabs {font-size: 18px} "
+      )
+    ),
+    tabsetPanel(
+      type = "tabs",
+      tabPanel(
+        "City",
+        uiOutput("city"),
+        plotlyOutput("donut_plot"),
+        verbatimTextOutput("txtOutput2")
       ),
-
-
-      ## Tabs Panel
-      tags$head(
-        tags$style(
-          type = "text/css",
-          ".nav-tabs {font-size: 18px} "
-        )
+      tabPanel(
+        "Zipcode",
+        plotOutput("sd_map"),
+        verbatimTextOutput("txtOutput3")
       ),
-      tabsetPanel(
-        type = "tabs",
-        tabPanel(
-          "Nature of Payments, by City",
-          uiOutput("city"),
-          plotlyOutput("donut_plot"),
-          verbatimTextOutput("txtOutput2")
-        ),
-        tabPanel(
-          "Totaled Payment Amounts, by Zipcode",
-          plotOutput("sd_map"),
-          verbatimTextOutput("txtOutput3")
-        ),
-        tabPanel(
-          "Total Payment and Payment Type",
-          verbatimTextOutput("Emmatxt")
-
-        ),        
-        tabPanel(
-          "Payment Summaries",
-          verbatimTextOutput("Gracetxt")
-        )
+      
+      tabPanel("Country",
+               sidebarLayout(
+                 sidebarPanel(
+                   titlePanel("Payments by Country"),
+                   selectInput("predictors", h3("Select Variable"),
+                               choices = c('Physician_Primary_Type',
+                                           'Related_Product_Indicator',
+                                           'Charity_Indicator',
+                                           'Form_of_Payment_or_Transfer_of_Value')
+                   )
+                 ),
+                 mainPanel(plotOutput("country"))
+               ),
+               verbatimTextOutput("txtOutput4")
       ),
-      width = 12
-    )
+      tabPanel(
+        "Total & Type",
+        verbatimTextOutput("Emmatxt")
+      ),        
+      tabPanel(
+        "Summaries",
+        uiOutput("SelectYear"),
+        dataTableOutput("Grace_table"),
+        verbatimTextOutput("Gracetxt")
+      ),
+      tabPanel(
+        "About",
+        verbatimTextOutput("Abouttxt")
+      )
+    ),
+    width = 12
   )
 )
